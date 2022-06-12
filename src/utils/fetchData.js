f7ffc60bd848e9494fcc35fd94ff7b0896e9ea67
Amazigh Hettal <amazighhettal@gmail.com>
2022-06-12 15:11:31 +0200

    first commit 1

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '7fa419b367msh9e0f8fedb9798b1p1e6c2bjsnb68f32fb382c',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
   },
 };
@@ -9,7 +9,7 @@ export const youtubeOptions = {
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '7fa419b367msh9e0f8fedb9798b1p1e6c2bjsnb68f32fb382c',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
   },
 };
